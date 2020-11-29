a76ab425de035d40ccdd2f6fb5cd3f7bbc4ec0b7
Denis <erami.ukr@gmail.com>
2020-11-29 09:08:31 +0200

    Initial Commit

---
+++ FoodCert-mobile/FoodCert_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodCert_mobileApp.swift
+//  FoodCert-mobile
+//
+//  Created by Denis on 11/29/20.
+//
+
+import SwiftUI
+
+@main
+struct FoodCert_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
